fa602708f386f4e37af05dc53f5d9e4f9de8cda5
MakotoInomata <inomar1486@gmail.com>
2018-05-27 00:27:12 +0900

    feat sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   root 'home#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+      passwords: 'users/passwords'
+  }
+  resources :users, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
